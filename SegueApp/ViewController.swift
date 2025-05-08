4c262b8ad40fea3afa6b25b4a978cdde79e65a22
1140074 <alejandro.guerrah@elektra.com.mx>
2025-05-08 10:14:21 -0600

    Initial Commit

---
+++ SegueApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegueApp
+//
+//  Created by Alejandro Guerra Hernandez on 08/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
