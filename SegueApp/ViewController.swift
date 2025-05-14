4ce411c7dc2944ab9d01674e91630df156912e2a
1140074 <alejandro.guerrah@elektra.com.mx>
2025-05-13 18:38:00 -0600

    ultimo commit

--- SegueApp/ViewController.swift
+++ SegueApp/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var nameText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Lifecycle
+        print("view did load")
+        nameText.text = "" // do it once time, seems nothing
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("view did appear")
+        nameText.text = "" // do it several times
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("view will appear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("view did disappear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("view will disappear")
     }
 
-
+    @IBAction func nextClicked(_ sender: Any) {
+        
+        let name = nameText.text!
+        performSegue(withIdentifier: "toSecondVC", sender: name)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "toSecondVC":
+            let destination = segue.destination as! SecondViewController
+            destination.myName = sender as! String
+        default:
+            break
+        }
+    }
 }
 
